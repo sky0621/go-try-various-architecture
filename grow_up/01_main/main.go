3a83433c1137628279195163a65c183d6e17893b
sky0621 <sky14work@gmail.com>
2019-04-24 09:18:36 +0900

    continue

--- grow_up/01_main/main.go
+++ grow_up/01_main/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"net/http"
 	"sync"
-	"time"
 
 	"github.com/labstack/echo"
 )
 
+// 注意: プロダクション品質ではありません。
 func main() {
 	e := echo.New()
 
@@ -16,21 +16,25 @@ func main() {
 
 	// 「商品」を登録
 	e.POST("/item", func(c echo.Context) error {
-		i := &item{}
+		i := &item{} // バリデーションは後回し
 		if err := c.Bind(i); err != nil {
-			return c.JSON(http.StatusBadRequest, nil)
+			return sendResponse(c, http.StatusBadRequest)
 		}
 		if _, ok := itemMap.Load(i.ID); ok {
-			return c.JSON(http.StatusBadRequest, nil)
+			return sendResponse(c, http.StatusBadRequest)
 		}
 		itemMap.Store(i.ID, i)
-		return c.JSON(http.StatusOK, nil)
+		return sendResponse(c, http.StatusOK)
 	})
 
 	// 「商品」一覧を返却
 	e.GET("/item", func(c echo.Context) error {
-		// FIXME:
-		return c.JSON(http.StatusOK, nil)
+		var res []*item
+		itemMap.Range(func(k, v interface{}) bool {
+			res = append(res, v.(*item))
+			return true
+		})
+		return c.JSON(http.StatusOK, res)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
@@ -38,8 +42,14 @@ func main() {
 
 // 「商品」を定義
 type item struct {
-	ID        string    `json:"id"`         // 商品ID
-	Name      string    `json:"name"`       // 商品名
-	Price     int       `json:"price"`      // 金額
-	CreatedAt time.Time `json:"created_at"` // 商品登録日
+	ID        string `json:"id"`         // 商品ID
+	Name      string `json:"name"`       // 商品名
+	Price     int    `json:"price"`      // 金額
+	CreatedAt int    `json:"created_at"` // 商品登録日(Unixタイムスタンプ)
+}
+
+func sendResponse(c echo.Context, code int) error {
+	return c.JSON(code, struct {
+		Message string `json:"message"`
+	}{Message: http.StatusText(code)})
 }
